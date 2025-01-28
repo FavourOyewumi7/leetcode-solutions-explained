30af0bb6277489e3b4ae90fdce7955a00aace44b
FavourOyewumi7 <oyewumifavour65@gmail.com>
2025-01-28 22:08:28 +0100

    isomorphic string solution

--- isomorphic_strings.py
+++ isomorphic_strings.py
@@ -1,3 +1,13 @@
+"""
+thought process:
+
+1. check if the length of the strings are the same, if not return false
+2. create two dictionaries, one to keep track of the mapping from s to t, and one to keep track of the mapping from t to s
+3. iterate through the strings, and for each character, check if it is in the dictionary, if not, add it to the dictionary
+4. if it is in the dictionary, check if the mapping is correct, if not return false
+5. if the mapping is correct, return true
+
+"""
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         
